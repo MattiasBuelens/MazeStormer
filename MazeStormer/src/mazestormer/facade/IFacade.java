f9b3a3900dfcebf1772372fb533595e3dc555d8a
Matthias <Matthias@Matthias-PC>
2012-10-02 17:52:00 +0200

    *New project *Partial package architecture *Control panel + facade
    @note: just a quick example of swing
    @note: the 'workpane' depends on the assignment is not yet implemented
    @note: the res file can only become smaller...

---
+++ MazeStormer/src/mazestormer/facade/IFacade.java
@@ -0,0 +1,14 @@
+package mazestormer.facade;
+
+/**
+ * An interface that should be implemented by all classes
+ * which must provide a UI Interaction.
+ * 
+ * @author 	Team Bronze
+ * @version	
+ *
+ */
+public interface IFacade{
+
+	public String getFeedback();
+}
