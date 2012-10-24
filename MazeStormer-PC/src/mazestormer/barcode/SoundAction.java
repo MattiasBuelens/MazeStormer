802b3e7ec2e4d171fa4e2016bf7359d3fd6332ae
matt77hias <matthias.moulin@gmail.com>
2012-10-24 17:25:52 +0200

    default acties

---
+++ MazeStormer-PC/src/mazestormer/barcode/SoundAction.java
@@ -0,0 +1,13 @@
+package mazestormer.barcode;
+
+import mazestormer.robot.Robot;
+
+public class SoundAction implements Action{
+
+	@Override
+	public void performAction(Robot robot) throws IllegalStateException {
+		if(robot == null)
+			throw new IllegalStateException("The given robot must be a valid robot.");
+	}
+
+}
