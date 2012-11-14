9594e0eaa40f1d04cc063d5246c6c49b2dd1bedc
Mattias Buelens <mattias@buelens.com>
2012-11-14 10:45:30 +0100

    Now using ButtonListener for escape button to stop the NXT program.

--- MazeStormer-NXT/src/mazestormer/remote/Program.java
+++ MazeStormer-NXT/src/mazestormer/remote/Program.java
@@ -3,16 +3,19 @@ package mazestormer.remote;
 import java.io.IOException;
 
 import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
 import lejos.nxt.NXT;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
 import mazestormer.robot.Robot;
 
-public class Program implements Runnable {
+public class Program implements Runnable, ButtonListener {
 
 	private Robot robot;
 	private NXTCommunicator communicator;
+
+	private boolean isRunning = false;
 
 	public static void main(String[] args) throws Exception {
 		new Program().run();
@@ -21,9 +24,12 @@ public class Program implements Runnable {
 
 	@Override
 	public void run() {
+		isRunning = true;
+		Button.ESCAPE.addButtonListener(this);
+
 		// Wait for Bluetooth connection from PC
-		NXTConnection connection = Bluetooth.waitForConnection();
 		println("Connecting");
+		NXTConnection connection = Bluetooth.waitForConnection();
 		if (connection != null) {
 			println("Connected");
 			println("ESC to quit");
@@ -35,18 +41,37 @@ public class Program implements Runnable {
 			communicator.start();
 		}
 
-		// Exit when pressing escape
-		Button.ESCAPE.waitForPress();
-		if (communicator != null) {
-			// Disconnect
-			println("Disconnecting");
+		while (isRunning)
+			Thread.yield();
+	}
+
+	public void stop() {
+		println("Disconnecting");
+		if (robot != null) {
 			robot.terminate();
+		}
+		if (communicator != null) {
 			try {
 				communicator.terminate();
 			} catch (IOException e) {
 			}
-			println("Disconnected");
 		}
+		println("Disconnected");
+		isRunning = false;
+	}
+
+	@Override
+	public void buttonPressed(Button b) {
+		switch (b.getId()) {
+		case Button.ID_ESCAPE:
+			stop();
+			break;
+		default:
+		}
+	}
+
+	@Override
+	public void buttonReleased(Button b) {
 	}
 
 	private int cursorY = 0;
