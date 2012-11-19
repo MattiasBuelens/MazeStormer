a2066bb65850c15eff6a8834a36bab5d874e19c8
matt77hias <matthias.moulin@gmail.com>
2012-11-19 21:43:05 +0100

    tile->node omzetting

--- MazeStormer-PC/src/mazestormer/maze/Maze.java
+++ MazeStormer-PC/src/mazestormer/maze/Maze.java
@@ -15,6 +15,7 @@ import lejos.geom.Point;
 import lejos.geom.Rectangle;
 import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.Pose;
+import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.AstarSearchAlgorithm;
 import lejos.robotics.pathfinding.FourWayGridMesh;
 import lejos.robotics.pathfinding.GridNode;
@@ -386,47 +387,59 @@ public class Maze extends AbstractEventSource {
 		return heading;
 	}
 	
-	@Deprecated
-	public Path findPath(Tile startTile, Tile goalTile){
-		float gridspace = getTileSize();
-		float clearance = getTileSize()/2;
-		Line[] lines = getLines().toArray(new Line[0]);
-		
-		LineMap map = new LineMap(lines, getBoundingRectangle());
-		FourWayGridMesh mesh = new FourWayGridMesh(map, gridspace, clearance);
-		
-		System.out.println("Size: " + mesh.getMesh().size());
-		for(Node n : mesh.getMesh())
-			System.out.println(" X: " + n.x + " Y: " + n.y + " N: " + n.neighbors());		
-		
+//	@Deprecated
+//	public Path findPathh(Tile startTile, Tile goalTile){
+//		float gridspace = getTileSize();
+//		float clearance = getTileSize()/2;
+//		Line[] lines = getLines().toArray(new Line[0]);
+//		
+//		LineMap map = new LineMap(lines, getBoundingRectangle());
+//		FourWayGridMesh mesh = new FourWayGridMesh(map, gridspace, clearance);
+//		
+//		System.out.println("Size: " + mesh.getMesh().size());
+//		for(Node n : mesh.getMesh())
+//			System.out.println(" X: " + n.x + " Y: " + n.y + " N: " + n.neighbors());		
+//		
 //		Node startNode = getClosestNodeOfTile(mesh.getMesh(),startTile);
 //		Node goalNode = getClosestNodeOfTile(mesh.getMesh(),goalTile);
 //		
 //		AstarSearchAlgorithm astar = new AstarSearchAlgorithm();
 //		return astar.findPath(startNode, goalNode);
-		return null;
+//		return null;
+//	}
+//	
+//	@Deprecated
+//	private Node getClosestNodeOfTile(Collection<Node> nodes, Tile tile){
+//		Node closest = new GridNode(100000,100000, getTileSize());
+//		for(Node node : nodes)
+//			if(Math.sqrt(Math.pow(Math.abs(tile.getX()-node.x),2)+Math.pow(Math.abs(tile.getY()-node.y),2))
+//					< Math.sqrt(Math.pow(Math.abs(tile.getX()-closest.x),2)+Math.pow(Math.abs(tile.getY()-closest.y),2)))
+//				closest = node;
+//		return closest;
+//	}
+	
+	
+	public Path findPath(Tile startTile, Tile goalTile){
+		Map<Tile, Node> nodes = getMesh();
+		Node startNode = nodes.get(startTile);
+		Node goalNode = nodes.get(goalTile);
+		
+		AstarSearchAlgorithm astar = new AstarSearchAlgorithm();
+		return astar.findPath(startNode, goalNode);
 	}
 	
-	@Deprecated
-	private Node getClosestNodeOfTile(Collection<Node> nodes, Tile tile){
-		Node closest = new GridNode(100000,100000, getTileSize());
-		for(Node node : nodes)
-			if(Math.sqrt(Math.pow(Math.abs(tile.getX()-node.x),2)+Math.pow(Math.abs(tile.getY()-node.y),2))
-					< Math.sqrt(Math.pow(Math.abs(tile.getX()-closest.x),2)+Math.pow(Math.abs(tile.getY()-closest.y),2)))
-				closest = node;
-		return closest;
-	}
-	
-	@Deprecated
 	private Map<Tile, Node> getMesh(){
 		Map<Tile, Node> nodes = new HashMap<Tile, Node>();
 		for(Tile tile : getTiles())
-			nodes.put(tile, new GridNode(tile.getX()+getTileSize()/2, tile.getY()+getTileSize()/2, getTileSize()));
+			nodes.put(tile, new Node(tile.getX(), tile.getY()));
 		for(Tile tile : getTiles()){
 			Node node = nodes.get(tile);
 			for(int i=0; i<Orientation.values().length; i++)
-				if(tile.hasEdgeAt(Orientation.values()[i]))
-					node.addNeighbor(nodes.get(getNeighbor(tile, Orientation.values()[i])));
+				if(tile.hasEdgeAt(Orientation.values()[i])){
+					Tile nb = getNeighbor(tile, Orientation.values()[i]);
+					if(nb != null)
+						node.addNeighbor(nodes.get(nb));
+				}
 		}
 		return nodes;
 	}
