e7cccffb27ebd86e0d970c16a40795afdedcb9d0
Mattias Buelens <mattias@buelens.com>
2012-11-20 18:16:46 +0100

    Iets properdere generatie van buren voor Mesh.

--- MazeStormer-PC/src/mazestormer/maze/Mesh.java
+++ MazeStormer-PC/src/mazestormer/maze/Mesh.java
@@ -9,85 +9,100 @@ import lejos.robotics.pathfinding.Path;
 import mazestormer.maze.parser.FileUtils;
 import mazestormer.maze.parser.Parser;
 
-public class Mesh{
-	
-	public static void main(String args[]) throws Exception{
+public class Mesh {
+
+	public static void main(String args[]) throws Exception {
 		Mesh mesh = new Mesh(new Maze());
 		mesh.setUpTest();
 		mesh.generateNodes();
 		mesh.printNodeMap();
 		System.out.println(mesh.findPath());
 	}
-	
+
 	@Deprecated
-	public void setUpTest() throws Exception{
-		String mazeFilePath = "C:/Users/Matthias/git/MazeStormer/MazeStormer-PC/src/res/mazes/Semester1_Demo2.txt";
+	public void setUpTest() throws Exception {
+		String mazeFilePath = Mesh.class.getResource(
+				"/res/mazes/Semester1_Demo2.txt").getPath();
 		CharSequence contents;
 		contents = FileUtils.load(mazeFilePath);
 		this.maze.clear();
 		new Parser(this.maze).parse(contents);
 	}
-	
+
 	@Deprecated
-	public Path findPath(){
+	public Path findPath() {
 		Tile startTile = (Tile) this.maze.getTiles().toArray()[11];
 		Tile goalTile = (Tile) this.maze.getTiles().toArray()[12];
-		
-		System.out.println("Start Tile X: " + startTile.getX() + "\t" + " Y: " + startTile.getY() + "\t" + " | Borders: " + startTile.getEdges().size() + " | Openings: " + startTile.getOpenSides().size());
-		System.out.println("Goal Tile X: " + goalTile.getX() + "\t" + " Y: " + goalTile.getY() + "\t" + " | Borders: " + goalTile.getEdges().size() + " | Openings: " + goalTile.getOpenSides().size());		
-		
+
+		System.out.println("Start Tile\tX: " + startTile.getX() + "\t" + " Y: "
+				+ startTile.getY() + "\t" + " | Borders: "
+				+ startTile.getClosedSides().size() + " | Openings: "
+				+ startTile.getOpenSides().size());
+		System.out.println("Goal Tile\tX: " + goalTile.getX() + "\t" + " Y: "
+				+ goalTile.getY() + "\t" + " | Borders: "
+				+ goalTile.getClosedSides().size() + " | Openings: "
+				+ goalTile.getOpenSides().size());
+
 		Node startNode = nodeMap.get(startTile);
 		Node goalNode = nodeMap.get(goalTile);
-		
+
 		AstarSearchAlgorithm astar = new AstarSearchAlgorithm();
 		return astar.findPath(startNode, goalNode);
 	}
-	
-	private Mesh(Maze maze){
+
+	private Mesh(Maze maze) {
 		setMaze(maze);
 	}
-	
+
 	private Maze maze;
-	
-	public void setMaze(Maze maze){
+
+	public void setMaze(Maze maze) {
 		this.maze = maze;
 	}
-	
-	public Maze getMaze(){
+
+	public Maze getMaze() {
 		return this.maze;
 	}
-	
+
 	private Map<Tile, Node> nodeMap = new HashMap<Tile, Node>();
 
-	public Path findPath(Tile startTile, Tile goalTile){
+	public Path findPath(Tile startTile, Tile goalTile) {
 		Node startNode = nodeMap.get(startTile);
 		Node goalNode = nodeMap.get(goalTile);
-		
+
 		AstarSearchAlgorithm astar = new AstarSearchAlgorithm();
 		return astar.findPath(startNode, goalNode);
 	}
-	
-	private void generateNodes(){
+
+	private void generateNodes() {
+		// Create nodes
 		Map<Tile, Node> nodes = new HashMap<Tile, Node>();
-		for(Tile tile : getMaze().getTiles())
+		for (Tile tile : getMaze().getTiles()) {
 			nodes.put(tile, new Node(tile.getX(), tile.getY()));
-		for(Tile tile : getMaze().getTiles()){
+		}
+		// Link nodes
+		for (Tile tile : getMaze().getTiles()) {
 			Node node = nodes.get(tile);
-			for(int i=0; i<Orientation.values().length; i++)
-				if(tile.hasEdgeAt(Orientation.values()[i])){
-					Tile nb = getMaze().getNeighbor(tile, Orientation.values()[i]);
-					if(nb != null)
-						node.addNeighbor(nodes.get(nb));
+			for (Orientation openSide : tile.getOpenSides()) {
+				Tile nb = getMaze().getNeighbor(tile, openSide);
+				if (nb != null) {
+					node.addNeighbor(nodes.get(nb));
 				}
+			}
 		}
 		this.nodeMap = nodes;
 	}
-	
-	public void printNodeMap(){
-		for(Tile tile : this.nodeMap.keySet()){
-			System.out.println("Tile X: " + tile.getX() + "\t" + " Y: " + tile.getY() + "\t" + " | Borders: " + tile.getEdges().size() + " | Openings: " + tile.getOpenSides().size());
+
+	public void printNodeMap() {
+		for (Tile tile : this.nodeMap.keySet()) {
+			System.out.println("Tile X: " + tile.getX() + "\t" + " Y: "
+					+ tile.getY() + "\t" + " | Borders: "
+					+ tile.getClosedSides().size() + " | Openings: "
+					+ tile.getOpenSides().size());
 			Node node = this.nodeMap.get(tile);
-				System.out.println("Node X: " + node.x + "\t" + " Y: " + node.y + "\t" + " | Borders: " + node.getNeighbors().size() + " | Openings: " + (4-node.getNeighbors().size()));
+			System.out.println("Node X: " + node.x + "\t" + " Y: " + node.y
+					+ "\t" + " | Borders: " + node.getNeighbors().size()
+					+ " | Openings: " + (4 - node.getNeighbors().size()));
 		}
 	}
 }
