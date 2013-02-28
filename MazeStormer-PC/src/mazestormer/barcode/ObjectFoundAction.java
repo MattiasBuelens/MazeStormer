e58378e92a901d480f0939540ba4a39bcc703e84
matt77hias <matthias.moulin@gmail.com>
2013-02-28 13:04:38 +0100

    Nieuwe barcodes: nog lege impl. nog verder info omtrent barcodes

---
+++ MazeStormer-PC/src/mazestormer/barcode/ObjectFoundAction.java
@@ -0,0 +1,15 @@
+package mazestormer.barcode;
+
+import mazestormer.maze.Maze;
+import mazestormer.robot.ControllableRobot;
+import mazestormer.util.Future;
+
+public class ObjectFoundAction implements IAction {
+
+	@Override
+	public Future<?> performAction(ControllableRobot robot, Maze maze) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ MazeStormer-PC/src/mazestormer/barcode/SeesawAction.java
@@ -0,0 +1,15 @@
+package mazestormer.barcode;
+
+import mazestormer.maze.Maze;
+import mazestormer.robot.ControllableRobot;
+import mazestormer.util.Future;
+
+public class SeesawAction implements IAction {
+
+	@Override
+	public Future<?> performAction(ControllableRobot robot, Maze maze) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
