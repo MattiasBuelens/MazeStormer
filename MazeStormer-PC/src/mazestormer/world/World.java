8a0c7da35a69f270737982ebf730be247feee6be
Mattias Buelens <mattias@buelens.com>
2013-03-18 17:50:14 +0100

    Actually added World classes.

---
+++ MazeStormer-PC/src/mazestormer/world/World.java
@@ -0,0 +1,59 @@
+package mazestormer.world;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import mazestormer.maze.Maze;
+import mazestormer.player.Player;
+
+public class World {
+
+	private final Maze maze = new Maze();
+	private final Set<Player> players = new HashSet<Player>();
+
+	private final List<WorldListener> listeners = new ArrayList<WorldListener>();
+
+	public Maze getMaze() {
+		return maze;
+	}
+
+	public Collection<Player> getPlayers() {
+		return Collections.unmodifiableSet(players);
+	}
+
+	public Player getPlayer(String playerID) {
+		for (Player player : players) {
+			if (player.getPlayerID().equals(playerID)) {
+				return player;
+			}
+		}
+		return null;
+	}
+
+	public void addPlayer(Player player) {
+		players.add(player);
+		for (WorldListener listener : listeners) {
+			listener.playerAdded(player);
+		}
+	}
+
+	public void removePlayer(Player player) {
+		players.remove(player);
+		for (WorldListener listener : listeners) {
+			listener.playerRemoved(player);
+		}
+	}
+
+	public void addListener(WorldListener listener) {
+		listeners.add(listener);
+	}
+
+	public void removeListener(WorldListener listener) {
+		listeners.remove(listener);
+	}
+
+}
---
+++ MazeStormer-PC/src/mazestormer/world/WorldListener.java
@@ -0,0 +1,11 @@
+package mazestormer.world;
+
+import mazestormer.player.Player;
+
+public interface WorldListener {
+
+	public void playerAdded(Player player);
+
+	public void playerRemoved(Player player);
+
+}
