8f93f8e2f9543ee05e9f2318877e39bb1cefc455
Mattias Buelens <mattias@buelens.com>
2013-03-31 21:26:13 +0200

    Restyled state panel using HTML goodness.

--- MazeStormer-PC/src/mazestormer/ui/StatePanel.java
+++ MazeStormer-PC/src/mazestormer/ui/StatePanel.java
@@ -28,14 +28,14 @@ public class StatePanel extends ViewPanel {
 	public StatePanel(IStateController controller) {
 		this.controller = controller;
 
-		setBorder(new TitledBorder(null, "Current state", TitledBorder.LEADING,
-				TitledBorder.TOP, null, null));
+		setBorder(new TitledBorder(null, "Current state", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setLayout(new BorderLayout(0, 0));
 
 		table = new JTable();
 		add(table);
 
 		textPane = new JTextPane();
+		textPane.setContentType("text/html");
 		textPane.setEditable(false);
 		add(textPane, BorderLayout.CENTER);
 
@@ -58,58 +58,67 @@ public class StatePanel extends ViewPanel {
 
 	@Subscribe
 	public void onMove(MoveEvent e) {
+		StringBuilder sb = new StringBuilder();
+		// Font
+		sb.append("<html><body style=\"font:");
+		sb.append(getFont().getSize()).append("pt ");
+		sb.append(getFont().getFamily());
+		sb.append("\">");
+		// Pose
 		String x = String.format("%.2f", controller.getXPosition());
 		String y = String.format("%.2f", controller.getYPosition());
 		String heading = String.format("%.2f", controller.getHeading());
-		String text = "POSE BEFORE ASSIGNMENT:" + "\n   x-position= " + x
-				+ "\n   y-position= " + y + "\n   heading= " + heading + "\n"
-				+ "\nCURRENT ASSIGNMENT: \n   ";
-
-		if (e.getEventType() == EventType.STOPPED) {
-			text = text + "stand still";
-		} else if (e.getEventType() == EventType.STARTED) {
-			Move move = e.getMove();
-			switch (move.getMoveType()) {
-			case TRAVEL:
-				float speed = Math.round(Math.abs(move.getTravelSpeed()) * 10) / 10;
-				if (move.getDistanceTraveled() == Float.POSITIVE_INFINITY)
-					text = text + "translate forward at " + speed + "cm/s";
-				else if (move.getDistanceTraveled() == Float.NEGATIVE_INFINITY)
-					text = text + "translate backward at " + speed + "cm/s";
-				else if (move.getTravelSpeed() >= 0)
-					text = text + "translate "
-							+ Math.round(Math.abs(move.getDistanceTraveled() * 10) / 10)
-							+ "cm forward at " + speed + "cm/s";
-				else if (move.getTravelSpeed() < 0)
-					text = text + "translate "
-							+ Math.round(Math.abs(move.getDistanceTraveled() * 10) / 10)
-							+ "cm backward at " + speed + "cm/s";
-				break;
-			case ROTATE:
-				float rotateSpeed = Math
-						.round(Math.abs(move.getRotateSpeed()) * 10) / 10;
-				if (move.getAngleTurned() == Float.POSITIVE_INFINITY)
-					text = text + "rotate counter-clockwise at " + rotateSpeed
-							+ "deg/s";
-				else if (move.getAngleTurned() == Float.NEGATIVE_INFINITY)
-					text = text + "rotate clockwise at " + rotateSpeed
-							+ "deg/s";
-				else if (move.getRotateSpeed() >= 0)
-					text = text + "rotate " + Math.round(Math.abs(move.getAngleTurned() * 10) / 10)
-							+ "deg counter-clockwise at " + rotateSpeed
-							+ "deg/s";
-				else if (move.getRotateSpeed() < 0)
-					text = text + "rotate " + Math.round(Math.abs(move.getAngleTurned() * 10) / 10)
-							+ "deg clockwise at " + rotateSpeed + "deg/s";
-				break;
-			default:
-				// n/a
+		sb.append("<strong>Pose before move:</strong><table>");
+		sb.append("<tr><th>X</th><td>").append(x).append("</td></tr>");
+		sb.append("<tr><th>Y</th><td>").append(y).append("</td></tr>");
+		sb.append("<tr><th>Heading</th><td>").append(heading).append("°</dd></td></tr></table>");
+		// Move
+		Move move = e.getMove();
+		if (move != null) {
+			sb.append("<strong>Move:</strong> ");
+			if (e.getEventType() == EventType.STOPPED) {
+				sb.append("stand still");
+			} else {
+				switch (move.getMoveType()) {
+				case TRAVEL:
+					float travelSpeed = move.getTravelSpeed();
+					float distance = move.getDistanceTraveled();
+					sb.append("translate ");
+					// Direction
+					sb.append(distance >= 0 ? "forward" : "backward");
+					// Distance
+					if (!Float.isInfinite(distance)) {
+						distance = Math.round(Math.abs(distance * 10) / 10);
+						sb.append(" ").append(distance).append("cm");
+					}
+					// Speed
+					travelSpeed = Math.round(Math.abs(travelSpeed) * 10) / 10;
+					sb.append(" at ").append(travelSpeed).append(" cm/s");
+					break;
+				case ROTATE:
+					float rotateSpeed = move.getRotateSpeed();
+					float angle = move.getAngleTurned();
+					boolean isCCW = angle >= 0;
+					sb.append("rotate ");
+					// Angle
+					if (!Float.isInfinite(angle)) {
+						angle = Math.round(Math.abs(angle * 10) / 10);
+						sb.append(angle).append("° ");
+					}
+					// Direction
+					sb.append(isCCW ? "counter-clockwise" : "clockwise");
+					// Speed
+					rotateSpeed = Math.round(Math.abs(rotateSpeed) * 10) / 10;
+					sb.append(" at ").append(rotateSpeed).append("°/s");
+					break;
+				default:
+					break;
+				}
 			}
-		} else {
-			text = text + "UNKNOWN";
 		}
+		sb.append("</body></html>");
 
-		textPane.setText(text);
+		textPane.setText(sb.toString());
 	}
 
 }
