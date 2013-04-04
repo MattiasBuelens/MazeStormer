ffdff1e15717a7cc29817081b08b8db4fe1def91
Mattias Buelens <mattias@buelens.com>
2013-04-04 18:00:13 +0200

    Added a few comments to TileTransform tests.

--- MazeStormer-PC/src/mazestormer/maze/TileTransformTest.java
+++ MazeStormer-PC/src/mazestormer/maze/TileTransformTest.java
@@ -11,10 +11,12 @@ public class TileTransformTest {
 	public void identicalTransform() {
 		TileTransform identicalTransform = new TileTransform(new LongPoint(0, 0), 0);
 
+		// In world coordinates
 		LongPoint origin = new LongPoint(0, 0);
 		LongPoint e_x = new LongPoint(1, 0);
 		LongPoint e_y = new LongPoint(0, 1);
 
+		// In relative coordinates
 		assertEquals(origin, identicalTransform.inverseTransform(origin));
 		assertEquals(e_x, identicalTransform.inverseTransform(e_x));
 		assertEquals(e_y, identicalTransform.inverseTransform(e_y));
@@ -24,10 +26,12 @@ public class TileTransformTest {
 	public void oneRotation() {
 		TileTransform transform = new TileTransform(new LongPoint(0, 0), 1);
 
+		// In world coordinates
 		LongPoint origin = new LongPoint(0, 0);
 		LongPoint e_x = new LongPoint(1, 0);
 		LongPoint e_y = new LongPoint(0, 1);
 
+		// In relative coordinates
 		assertEquals(new LongPoint(0, 0), transform.inverseTransform(origin));
 		assertEquals(new LongPoint(0, -1), transform.inverseTransform(e_x));
 		assertEquals(new LongPoint(1, 0), transform.inverseTransform(e_y));
@@ -37,10 +41,12 @@ public class TileTransformTest {
 	public void twoRotations() {
 		TileTransform transform = new TileTransform(new LongPoint(0, 0), 2);
 
+		// In world coordinates
 		LongPoint origin = new LongPoint(0, 0);
 		LongPoint e_x = new LongPoint(1, 0);
 		LongPoint e_y = new LongPoint(0, 1);
 
+		// In relative coordinates
 		assertEquals(new LongPoint(0, 0), transform.inverseTransform(origin));
 		assertEquals(new LongPoint(-1, 0), transform.inverseTransform(e_x));
 		assertEquals(new LongPoint(0, -1), transform.inverseTransform(e_y));
@@ -50,10 +56,12 @@ public class TileTransformTest {
 	public void translationOver32() {
 		TileTransform transform = new TileTransform(new LongPoint(3, 2), 0);
 
+		// In world coordinates
 		LongPoint origin = new LongPoint(0, 0);
 		LongPoint e_x = new LongPoint(1, 0);
 		LongPoint e_y = new LongPoint(0, 1);
 
+		// In relative coordinates
 		assertEquals(new LongPoint(-3, -2), transform.inverseTransform(origin));
 		assertEquals(new LongPoint(-2, -2), transform.inverseTransform(e_x));
 		assertEquals(new LongPoint(-3, -1), transform.inverseTransform(e_y));
@@ -63,10 +71,12 @@ public class TileTransformTest {
 	public void translationOverMinus3Minus2() {
 		TileTransform transform = new TileTransform(new LongPoint(-3, -2), 0);
 
+		// In world coordinates
 		LongPoint origin = new LongPoint(0, 0);
 		LongPoint e_x = new LongPoint(1, 0);
 		LongPoint e_y = new LongPoint(0, 1);
 
+		// In relative coordinates
 		assertEquals(new LongPoint(3, 2), transform.inverseTransform(origin));
 		assertEquals(new LongPoint(4, 2), transform.inverseTransform(e_x));
 		assertEquals(new LongPoint(3, 3), transform.inverseTransform(e_y));
@@ -76,11 +86,13 @@ public class TileTransformTest {
 	public void translationAndRotation() {
 		TileTransform transform = new TileTransform(new LongPoint(1, 2), 3);
 
+		// In world coordinates
 		LongPoint origin = new LongPoint(0, 0);
 		LongPoint e_x = new LongPoint(1, 0);
 		LongPoint e_y = new LongPoint(0, 1);
 		LongPoint P = new LongPoint(3, 4);
 
+		// In relative coordinates
 		assertEquals(new LongPoint(2, -1), transform.inverseTransform(origin));
 		assertEquals(new LongPoint(2, 0), transform.inverseTransform(e_x));
 		assertEquals(new LongPoint(1, -1), transform.inverseTransform(e_y));
