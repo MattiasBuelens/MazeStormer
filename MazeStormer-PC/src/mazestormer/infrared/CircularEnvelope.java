c68892750c4df0a9d4b701675d4ae585c84aee1d
matt77hias <matthias.moulin@gmail.com>
2013-04-15 19:35:49 +0200

    circular envelope

--- MazeStormer-PC/src/mazestormer/infrared/CircularEnvelope.java
+++ MazeStormer-PC/src/mazestormer/infrared/CircularEnvelope.java
@@ -3,6 +3,7 @@ package mazestormer.infrared;
 import java.awt.geom.Point2D;
 
 import lejos.robotics.navigation.Pose;
+import mazestormer.util.ArrayUtils;
 
 public class CircularEnvelope implements Envelope {
 
@@ -34,17 +35,34 @@ public class CircularEnvelope implements Envelope {
 	@Override
 	public Point2D[] getDiscretization(Pose pose, int depth)
 			throws IllegalArgumentException {
-		// TODO: dummy implementation
-		return (new RectangularEnvelope(2 * getRadius(), 2 * getRadius()))
-				.getDiscretization(pose, depth);
+		if (depth <= 0) {
+			throw new IllegalArgumentException(
+					"The given depth must be greater than zero.");
+		}
+
+		Point2D[] tps = new Point2D.Double[depth];
+		double d = (2*Math.PI / (double) depth);
+
+		for (int i = 0; i < depth; i++) {
+			tps[i] = getPointAtCircleParameter(pose, d);
+		}
+		return tps;
+	}
+	
+	private Point2D getPointAtCircleParameter(Pose pose, double u) {
+		float center_x = pose.getX();
+		float center_y = pose.getY();
+		double c_x = center_x + radius * Math.cos(u);
+		double c_y = center_y + radius * Math.sin(u);
+		return new Point2D.Double(c_x, c_y);
 	}
 
 	@Override
 	public Point2D[] getCombination(Pose pose, Point2D target, int depth)
 			throws IllegalArgumentException {
-		// TODO: dummy implementation
-		return (new RectangularEnvelope(2 * getRadius(), 2 * getRadius()))
-				.getCombination(pose, target, depth);
+		Point2D[] dps = getDiscretization(pose, depth);
+		Point2D[] cps = getClosestPoints(pose, target);
+		return ArrayUtils.concat(dps, cps);
 	}
 
 }
