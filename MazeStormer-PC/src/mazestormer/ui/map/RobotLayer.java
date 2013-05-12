420227a20b8ccc0240a4c1c0cb8253c36aed95f3
Mattias Buelens <mattias@buelens.com>
2013-05-12 17:25:42 +0200

    Give robots an outline based on their team name.
    If the team name was not recognized, a random color is used instead.

--- MazeStormer-PC/src/mazestormer/ui/map/RobotLayer.java
+++ MazeStormer-PC/src/mazestormer/ui/map/RobotLayer.java
@@ -1,20 +1,39 @@
 package mazestormer.ui.map;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
+import org.apache.batik.dom.svg.SVGStylableElement;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.svg.SVGDocument;
 
 public class RobotLayer extends TransformLayer {
 
 	private static final double defaultHeight = 15d;
+	private static final String outlineClass = "outline";
+	private static final String outlineStrokeWidth = "20";
 
 	private Element element;
+	private final String color;
 
 	public RobotLayer(String name, double height) {
+		this(name, height, getTeamColor(name));
+	}
+
+	public RobotLayer(String name, double height, String color) {
 		super(name);
 		setHeight(height);
 		setRotationCenter(0, 0);
+
+		if (color == null) {
+			color = getRandomColor();
+		}
+		this.color = color;
 	}
 
 	public RobotLayer(String name) {
@@ -27,6 +46,7 @@ public class RobotLayer extends TransformLayer {
 			// Import to document
 			if (element == null) {
 				element = (Element) getDocument().importNode(getRobot().getDocumentElement(), true);
+				applyOutlines(element);
 			}
 			// Return imported element
 			return element;
@@ -41,6 +61,25 @@ public class RobotLayer extends TransformLayer {
 		return 1000;
 	}
 
+	/**
+	 * Apply outline styling to the direct children of the given container with
+	 * the {@link #outlineClass} class name.
+	 */
+	private void applyOutlines(Element container) {
+		NodeList nodes = container.getChildNodes();
+		for (int i = 0; i < nodes.getLength(); ++i) {
+			Node node = nodes.item(i);
+			if (node instanceof SVGStylableElement) {
+				SVGStylableElement element = (SVGStylableElement) node;
+				if (element.getClassName().getBaseVal().contains(outlineClass)) {
+					element.setAttribute(SVG_STROKE_ATTRIBUTE, color);
+					element.setAttribute(SVG_STROKE_WIDTH_ATTRIBUTE, outlineStrokeWidth);
+				}
+			}
+		}
+
+	}
+
 	private static final URL robotUrl = RobotLayer.class.getResource("/res/images/robot.svg");
 	private static SVGDocument robot;
 
@@ -49,6 +88,50 @@ public class RobotLayer extends TransformLayer {
 			robot = SVGUtils.loadSVGDocument(robotUrl);
 		}
 		return robot;
+	}
+
+	/*
+	 * Team colors
+	 */
+
+	private static final Map<Pattern, String> teamColors = new HashMap<Pattern, String>();
+	static {
+		teamColors.put(teamPattern("paars"), CSS_VIOLET_VALUE);
+		teamColors.put(teamPattern("blauw"), CSS_BLUE_VALUE);
+		teamColors.put(teamPattern("rood"), CSS_BLUE_VALUE);
+		teamColors.put(teamPattern("groen"), CSS_GREEN_VALUE);
+		teamColors.put(teamPattern("wit"), CSS_WHITE_VALUE);
+		teamColors.put(teamPattern("geel"), CSS_YELLOW_VALUE);
+		teamColors.put(teamPattern("indigo"), CSS_INDIGO_VALUE);
+		teamColors.put(teamPattern("brons"), CSS_DARKORANGE_VALUE);
+		teamColors.put(teamPattern("zilver"), CSS_SILVER_VALUE);
+		teamColors.put(teamPattern("goud"), CSS_GOLD_VALUE);
+		teamColors.put(teamPattern("platinum"), CSS_GAINSBORO_VALUE);
+	}
+
+	private static Pattern teamPattern(String teamName) {
+		return Pattern.compile(teamName, Pattern.CASE_INSENSITIVE | Pattern.LITERAL);
+	}
+
+	private static String getTeamColor(String name) {
+		for (Map.Entry<Pattern, String> entry : teamColors.entrySet()) {
+			if (entry.getKey().matcher(name).find()) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * Random colors
+	 */
+
+	private static final String[] randomColors = new String[] { CSS_LIME_VALUE, CSS_CYAN_VALUE, CSS_HOTPINK_VALUE,
+			CSS_LIGHTSLATEGREY_VALUE, CSS_BLACK_VALUE };
+	private static final AtomicInteger nextRandomColor = new AtomicInteger(0);
+
+	private static String getRandomColor() {
+		return randomColors[nextRandomColor.getAndIncrement() % randomColors.length];
 	}
 
 }
